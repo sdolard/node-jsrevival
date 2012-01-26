6888975fc6fa6a73be9b0085849b95da2af604cc
Sébasten Dolard <sdolard@gmail.com>
2012-01-26 23:15:47 +0100

    test

---
+++ test/run_test.js
@@ -0,0 +1,40 @@
+var 
+fs = require('fs'),
+path = require("path"),
+RE_JS_FILE=/.*\.js$/i;
+RE_BENCH_FILE=/.*bench.*/i;
+
+fs.readdir(__dirname, function (err, files) {
+		var 
+		i, 
+		failures = 0, 
+		file;
+		
+		for(i = 0; i < files.length; i++) {
+			file = files[i];
+			
+			if (!RE_JS_FILE.test(file)) {
+				continue;
+			}	
+			if (file === "run_test.js") {
+				continue;
+			}
+			if (RE_BENCH_FILE.test(file)) {
+				continue;
+			}	
+			
+			try {
+				require(path.resolve(__dirname, file));
+				console.log("OK - %s ", file);
+			} catch (e) {
+				console.log("KO - %s ", file, e);
+				failures++;
+			}
+			
+		}
+		if (!failures) {
+			return console.log("#all pass");
+		}
+		
+		console.error("%d %s", failures, "failure" + (failures > 1 ? "s" : ""));
+});
